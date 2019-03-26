ff11a5819a60f296095c42fe436b0f92fb62f947
LuSuguru <429150413@qq.com>
2019-03-26 10:53:44 +0800

    resume mount class instance

--- src/react-fiber/class-component.ts
+++ src/react-fiber/class-component.ts
@@ -1,11 +1,11 @@
-import { isFunction } from 'util'
 import { computeExpirationTimeForFiber, requestCurrentTime, scheduleWork } from '../react-scheduler'
 import { Update as UpdateTag } from '../react-type/effect-type'
 import Update, { ForceUpdate, ReplaceState, UpdateState } from '../react-update/update'
 import { changeHasForceUpdate, enqueueUpdate, getHasForceUpdate, processUpdateQueue, UpdateQueue } from '../react-update/update-queue'
 import { Component } from '../react/react-component'
 import { ReactUpdateQueue } from '../react/react-noop-update-queue'
-import { isEmpty } from '../utils/getType'
+import { isEmpty, isFunction } from '../utils/getType'
+import { shallowEqual } from '../utils/lib'
 import { ExpirationTime, NoWork } from './expiration-time'
 import { Fiber } from './fiber'
 import { isMounted } from './fiber-tree-reflection'
@@ -87,7 +87,17 @@ function applyComponentWillReceiveProps(instance: any, newProps: any, nextContex
   }
 }
 
+function checkShouldComponentUpdate(instance: any, ctor: any, oldProps: any, newProps: any, oldState: any, newState: any, newContext: any) {
+  if (isFunction(instance.shouldComponentUpdate)) {
+    const shouldUpdate = instance.shouldComponentUpdate(newProps, newState, newContext)
+    return shouldUpdate
+  }
 
+  if (ctor.isPureReactComponent) {
+    !shallowEqual(oldProps, newProps) || !shallowEqual(oldState, newState)
+  }
+  return true
+}
 
 function addOptionClassInstace(workInProgress: Fiber, instance: Component) {
   instance.updater = classComponentUpdater
@@ -141,10 +151,9 @@ function mountClassInstance(workInProgress: Fiber, ctor: any, newProps: any, ren
     applyDerivedStateFromProps(workInProgress, getDerivedStateFromProps, newProps)
   }
 
-  if (
-    typeof ctor.getDerivedStateFromProps !== 'function' && typeof instance.getSnapshotBeforeUpdate !== 'function'
-    && (typeof instance.UNSAFE_componentWillMount === 'function' || typeof instance.componentWillMount === 'function')
-  ) {
+  const haveNewLifecycle = isFunction(ctor.getDerivedStateFromProps) && isFunction(instance.getSnapshotBeforeUpdate)
+  const haveComponentWillMount = isFunction(instance.UNSAFE_componentWillMount) && isFunction(instance.componentWillMount)
+  if (!haveNewLifecycle && haveComponentWillMount) {
     applyComponentWillMount(instance)
 
     updateQueue = workInProgress.updateQueue
@@ -182,32 +191,63 @@ function resumeMountClassInstance(workInProgress: Fiber, ctor: any, newProps: an
 
   const { getDerivedStateFromProps, getSnapshotBeforeUpdate } = ctor
 
-  const hasNewLifecycles = isFunction(getDerivedStateFromProps) || isFunction(getSnapshotBeforeUpdate)
-  const hasOldLifeCycles = isFunction(instance.UNSAFE_componentWillReceiveProps) || isFunction(instance.componentWillReceiveProps)
-  if (!hasNewLifecycles && hasOldLifeCycles) {
+  const haveNewLifecycles = isFunction(getDerivedStateFromProps) || isFunction(getSnapshotBeforeUpdate)
+  const havecomponentWillReceiveProps = isFunction(instance.UNSAFE_componentWillReceiveProps) || isFunction(instance.componentWillReceiveProps)
+  if (!haveNewLifecycles && havecomponentWillReceiveProps) {
     if (oldProps !== newProps || oldContext !== nextContext) {
       applyComponentWillReceiveProps(instance, newProps, nextContext)
     }
-
-    changeHasForceUpdate(false)
-
-
-    const oldState = workInProgress.memoizedState
-    let newState: any = (instance.state = oldState)
-    const updateQueue = workInProgress.updateQueue
-    if (updateQueue !== null) {
-      processUpdateQueue(workInProgress, updateQueue, newProps, instance, renderExpirationTime)
-      newState = workInProgress.memoizedState
-    }
-
-    const havaComponentDidMount = typeof()
-
-    if (oldProps === newProps && oldState === newState && !getHasForceUpdate()) { // !hasContextChanged()) {
-      if (typeof instance.componentDidMount === 'function') {
-        workInProgress.effectTag |= UpdateTag
-      }
-      return false
-    }
   }
+
+  changeHasForceUpdate(false)
+
+  const oldState = workInProgress.memoizedState
+  let newState: any = (instance.state = oldState)
+
+  const updateQueue = workInProgress.updateQueue
+  if (updateQueue !== null) {
+    processUpdateQueue(workInProgress, updateQueue, newProps, instance, renderExpirationTime)
+    newState = workInProgress.memoizedState
+  }
+
+  const haveComponentDidMount = isFunction(instance.componentDidMount)
+  if (oldProps === newProps && oldState === newState && !getHasForceUpdate()) { // !hasContextChanged()) {
+    if (haveComponentDidMount) {
+      workInProgress.effectTag |= UpdateTag
+    }
+    return false
+  }
+
+  if (isFunction(getDerivedStateFromProps)) {
+    applyDerivedStateFromProps(workInProgress, getDerivedStateFromProps, newProps)
+    newState = workInProgress.memoizedState
+  }
+
+  const shouldUpdate = getHasForceUpdate() || checkShouldComponentUpdate(instance, ctor, oldProps, newProps, oldState, newState, nextContext)
+
+  if (shouldUpdate) {
+    if (!haveNewLifecycles) {
+      if (isFunction(instance.componentWillReceiveProps)) {
+        instance.componentWillReceiveProps(newProps, nextContext)
+      }
+      if (isFunction(instance.UNSAFE_componentWillReceiveProps)) {
+        instance.UNSAFE_componentWillReceiveProps(newProps, nextContext)
+      }
+    }
+  } else {
+    workInProgress.memoizedProps = newProps
+    workInProgress.memoizedState = newState
+  }
+
+  if (haveComponentDidMount) {
+    workInProgress.effectTag |= UpdateTag
+  }
+
+  instance.props = newProps
+  instance.state = newState
+  instance.context = nextContext
+
+  return shouldUpdate
 }
+
 export { constructClassInstance, mountClassInstance, resumeMountClassInstance }
---
+++ src/utils/lib.ts
@@ -0,0 +1,25 @@
+export function shallowEqual(objA: any, objB: any): boolean {
+  if (Object.is(objA, objB)) {
+    return true
+  }
+
+  if (typeof objA !== 'object' || objA === null || typeof objB !== 'object' || objB === null) {
+    return false
+  }
+
+  const keysA = Object.keys(objA)
+  const keysB = Object.keys(objB)
+
+  if (keysA.length !== keysB.length) {
+    return false
+  }
+
+  for (let i = 0; i < keysA.length; i++) {
+    if (!Object.prototype.hasOwnProperty.call(objB, keysA[i]) || !Object.is(objA[keysA[i]], objB[keysA[i]])) {
+      return false
+    }
+  }
+
+  return true
+}
+
