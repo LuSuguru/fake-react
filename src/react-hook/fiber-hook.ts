4688054b834dcfc1868c8247666bbe4b7827cfb7
LuSuguru <429150413@qq.com>
2019-04-23 21:35:04 +0800

    add useState

--- src/react-hook/fiber-hook.ts
+++ src/react-hook/fiber-hook.ts
@@ -1,23 +1,114 @@
 
+import { readContext } from '../react-context/fiber-context'
 import { ExpirationTime, NoWork } from '../react-fiber/expiration-time'
 import { Fiber } from '../react-fiber/fiber'
 import { Passive, Update } from '../react-type/effect-type'
-import { UpdateQueue } from '../react-update/update-queue'
-
-export interface Hook {
-  memoizedState: any,
-  baseState: any,
-  baseUpdate: Update<any>,
-  queue: UpdateQueue<any>,
-  next: Hook,
-}
+import { BasicStateAction, Dispatch, Dispatcher, Hook } from '../react-type/hook-type'
+import { isFunction } from '../utils/getType'
+import ReactCurrentDispatcher from './rect-current-dispatcher'
 
 const didScheduleRenderPhaseUpdate: boolean = false
 
 let renderExpirationTime: ExpirationTime = NoWork
 let currentlyRenderingFiber: Fiber = null
-
 let nextCurrentHook: Hook = null
+
+let currentHook: Hook = null
+let nextCurrentHook: Hook = null
+
+let firstWorkInProgressHook: Hook = null
+let workInProgressHook: Hook = null
+let nextWorkInProgressHook: Hook = null
+
+function mountWorkInProgressHook(): Hook {
+  const hook: Hook = {
+    memoizedState: null,
+    baseState: null,
+    baseUpdate: null,
+    queue: null,
+    next: null,
+  }
+
+  if (workInProgressHook === null) {
+    firstWorkInProgressHook = workInProgressHook = hook // 第一次
+  } else {
+    workInProgressHook = workInProgressHook.next = hook // 插入链表中
+  }
+  return workInProgressHook
+}
+
+function updateWorkInProgressHook(): Hook {
+  if (nextWorkInProgressHook) {
+    workInProgressHook = nextWorkInProgressHook
+    nextWorkInProgressHook = workInProgressHook.next
+
+    currentHook = nextCurrentHook
+    nextCurrentHook = currentHook !== null ? currentHook.next : null
+  } else {
+    currentHook = nextCurrentHook
+
+    const newHook: Hook = {
+      memoizedState: currentHook.memoizedState,
+      baseState: currentHook.baseState,
+      queue: currentHook.queue,
+      baseUpdate: currentHook.baseUpdate,
+      next: null,
+    }
+
+    if (workInProgressHook === null) {
+      workInProgressHook = firstWorkInProgressHook = newHook // 第一次
+    } else {
+      workInProgressHook = workInProgressHook.next = newHook// 插入链表中
+    }
+    nextCurrentHook = currentHook.next
+  }
+  return workInProgressHook
+}
+
+const State = {
+  basicStateReducer<S>(state: S, action: BasicStateAction<S>): S {
+    return isFunction(action) ? (action as Function)(state) : action
+  },
+
+  mountState<S>(initialState: (() => S) | S): [S, Dispatch<BasicStateAction<S>>] {
+    const hook: Hook = mountWorkInProgressHook()
+    const queue
+
+    return null
+  },
+}
+
+
+
+const HooksDispatcherOnMount: Dispatcher = {
+  readContext,
+
+  useState: mountState,
+  useEffect: mountEffect,
+  useContext: readContext,
+
+  useReducer: mountReducer,
+  useCallback: mountCallback,
+  useMemo: mountMemo,
+  useRef: mountRef,
+  useLayoutEffect: mountLayoutEffect,
+}
+
+const HooksDispatcherOnUpdate: Dispatcher = {
+  readContext,
+
+  useState: updateState,
+  useEffect: updateEffect,
+  useContext: readContext,
+
+  useReducer: updateReducer,
+  useCallback: updateCallback,
+  useMemo: updateMemo,
+  useRef: updateRef,
+  useLayoutEffect: updateLayoutEffect,
+}
+
+
 
 function bailoutHooks(current: Fiber, workInProgress: Fiber, expirationTime: ExpirationTime) {
   workInProgress.updateQueue = current.updateQueue
@@ -30,10 +121,9 @@ function bailoutHooks(current: Fiber, workInProgress: Fiber, expirationTime: Exp
 function renderWithHooks(current: Fiber, workInProgress: Fiber, Component: Function, props: any, refOrContext: any, nextRenderExpirationTime: ExpirationTime): any {
   renderExpirationTime = nextRenderExpirationTime
   currentlyRenderingFiber = workInProgress
-
   nextCurrentHook = current !== null ? current.memoizedState : null
 
-  // ReactCurrentDispatcher.current = nextCurrentHook === null ? HooksDispatcherOnMount : HooksDispatcherOnUpdate
+  ReactCurrentDispatcher.current = nextCurrentHook === null ? HooksDispatcherOnMount : HooksDispatcherOnUpdate
 
   const children: any = Component(props, refOrContext)
   // 待实现
--- src/react-type/hook-type.ts
+++ src/react-type/hook-type.ts
@@ -1,7 +1,7 @@
 import { ReactContext } from '../react-context/fiber-context'
+import { ExpirationTime } from '../react-fiber/expiration-time'
 
-type Dispatch<A> = (a: A) => any
-
+type Dispatch<A> = (a: A) => void
 type BasicStateAction<S> = ((s: S) => S) | S
 
 interface Dispatcher {
@@ -11,13 +11,37 @@ interface Dispatcher {
   useContext<T>(context: ReactContext<T>, observedBits: void | number | boolean): T,
   useRef<T>(initialValue: T): { current: T },
   useEffect(create: () => (() => void) | void, deps: any[] | void | null): void,
-  useLayoutEffect(create: () => (() => void) | void, deps: any[] | void | null ): void,
+  useLayoutEffect(create: () => (() => void) | void, deps: any[] | void | null): void,
   useCallback<T>(callback: T, deps: any[] | void | null): T,
   useMemo<T>(nextCreate: () => T, deps: any[] | void | null): T
+}
+
+interface Update<S, A> {
+  expirationTime: ExpirationTime,
+  action: A,
+  eagerReducer: ((s: S, a: A) => S) | null,
+  eagerState: S | null,
+  next: Update<S, A> | null,
+}
+
+interface UpdateQueue<S, A> {
+  last: Update<S, A> | null,
+  dispatch: ((a: A) => any) | null,
+  eagerReducer: ((s: S, a: A) => S) | null,
+  eagerState: S | null,
+}
+
+interface Hook {
+  memoizedState: any,
+  baseState: any,
+  baseUpdate: Update<any, any>,
+  queue: UpdateQueue<any, any>,
+  next: Hook,
 }
 
 export {
   BasicStateAction,
   Dispatch,
   Dispatcher,
+  Hook,
 }
