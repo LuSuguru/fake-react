dcbb75c6445c832c8e5a0946c24fbc20c44ffd9d
LuSuguru <429150413@qq.com>
2019-04-25 21:29:23 +0800

    add use reducer

--- src/react-hook/fiber-hook.ts
+++ src/react-hook/fiber-hook.ts
@@ -5,11 +5,14 @@ import { Fiber } from '../react-fiber/fiber'
 import { computeExpirationTimeForFiber, requestCurrentTime, scheduleWork } from '../react-scheduler'
 import { Passive, Update as UpdateTag } from '../react-type/effect-type'
 import { BasicStateAction, Dispatch, Dispatcher, Hook, Update, UpdateQueue } from '../react-type/hook-type'
+import { markWorkInProgressReceivedUpdate } from '../react-work/begin-work'
 import { isFunction } from '../utils/getType'
 import ReactCurrentDispatcher from './rect-current-dispatcher'
 
 let didScheduleRenderPhaseUpdate: boolean = false
 let renderPhaseUpdates: Map<UpdateQueue<any, any>, Update<any, any>> = null
+
+const numberOfReRenders: number = 0
 
 let renderExpirationTime: ExpirationTime = NoWork
 let currentlyRenderingFiber: Fiber = null
@@ -20,6 +23,8 @@ let nextCurrentHook: Hook = null
 let firstWorkInProgressHook: Hook = null
 let workInProgressHook: Hook = null
 let nextWorkInProgressHook: Hook = null
+
+let remainingExpirationTime: ExpirationTime = NoWork
 
 function mountWorkInProgressHook(): Hook {
   const hook: Hook = {
@@ -137,28 +142,154 @@ function dispatchAction<S, A>(fiber: Fiber, queue: UpdateQueue<S, A>, action: A)
   }
 }
 
-function basicStateReducer<S>(state: S, action: BasicStateAction<S>): S {
-  return isFunction(action) ? (action as Function)(state) : action
-},
+
 
 const State = {
+  basicStateReducer<S>(state: S, action: BasicStateAction<S>): S {
+    return isFunction(action) ? (action as Function)(state) : action
+  },
+
   mountState<S>(initialState: (() => S) | S): [S, Dispatch<BasicStateAction<S>>] {
+    if (isFunction(initialState)) {
+      initialState = (initialState as Function)()
+    }
+    return Reducer.mountReducer(State.basicStateReducer, initialState)
+  },
+
+  updateState<S>(initialState: (() => S) | S): [S, Dispatch<BasicStateAction<S>>] {
+    return Reducer.updateReducer(State.basicStateReducer)
+  },
+}
+
+const Reducer = {
+  mountReducer<S, I, A>(reducer: (s: S, a: A) => S, initialArg: I, init?: (i: I) => S): [S, Dispatch<A>] {
     const hook: Hook = mountWorkInProgressHook()
+
+    let initialState: S = null
+    if (init !== null) {
+      initialState = init(initialArg)
+    } else {
+      initialState = initialArg as any
+    }
+
     hook.memoizedState = hook.baseState = initialState
     const queue = hook.queue = {
       last: null,
       dispatch: null,
-      eagerReducer: basicStateReducer,
+      eagerReducer: reducer,
       eagerState: initialState,
     }
 
-    const dispatch: Dispatch<BasicStateAction<S>> = queue.dispatch = dispatchAction.bind(null, currentlyRenderingFiber, queue)
-
+    const dispatch: Dispatch<A> = queue.dispatch = dispatchAction.bind(null, currentlyRenderingFiber, queue)
     return [hook.memoizedState, dispatch]
   },
 
-  updateState<S>(initialState: (() => S) | S): [S, Dispatch<BasicStateAction<S>>] {
-    return updateReducer(basicStateReducer, initialState)
+  updateReducer<S, A>(reducer: (s: S, a: A) => S): [S, Dispatch<A>] {
+    const hook = updateWorkInProgressHook()
+    const { queue } = hook
+
+    if (numberOfReRenders > 0) {
+      const { dispatch } = queue
+
+      if (renderPhaseUpdates !== null) {
+        const firstRenderPhaseUpdate = renderPhaseUpdates.get(queue)
+        if (firstRenderPhaseUpdate !== undefined) {
+          renderPhaseUpdates.delete(queue)
+
+          let newState = hook.memoizedState
+          let update = firstRenderPhaseUpdate
+          do {
+            const { action } = update.action
+            newState = reducer(newState, action)
+            update = update.next
+          } while (update !== null)
+
+          if (Object.is(newState, hook.memoizedState)) { // 标记为更新
+            markWorkInProgressReceivedUpdate()
+          }
+
+          hook.memoizedState = newState
+
+          if (hook.baseUpdate === queue.last) {
+            hook.baseState = newState
+          }
+
+          queue.eagerReducer = reducer
+          queue.eagerState = newState
+
+          return [newState, dispatch]
+        }
+      }
+
+      return [hook.memoizedState, dispatch]
+    }
+
+    const { last } = queue
+    const { baseState, baseUpdate } = hook
+
+    let first: Update<S, A> = null
+    if (baseUpdate !== null) {
+      if (last !== null) {
+        last.next = null
+      }
+      first = baseUpdate.next
+    } else {
+      first = last !== null ? last.next : null
+    }
+
+    if (first !== null) {
+      let newState: S = baseState
+      let prevUpdate: Update<S, A> = baseUpdate
+      let update: Update<S, A> = first
+
+      let newBaseState: S = null
+      let newBaseUpdate: Update<S, A> = null
+      let didSkip: boolean = false
+
+      do {
+        const updateExpirationTime: ExpirationTime = update.expirationTime
+        if (updateExpirationTime < renderExpirationTime) {
+          if (!didSkip) {
+            didSkip = true
+            newBaseState = newState
+            newBaseUpdate = prevUpdate
+          }
+
+          if (updateExpirationTime > remainingExpirationTime) {
+            remainingExpirationTime = updateExpirationTime
+          }
+        } else {
+          if (update.eagerReducer === reducer) {
+            newState = update.eagerState
+          } else {
+            const { action } = update
+            newState = reducer(newState, action)
+          }
+        }
+
+        prevUpdate = update
+        update = update.next
+      } while (update !== null && update !== first)
+
+      if (!didSkip) {
+        newBaseState = newState
+        newBaseUpdate = prevUpdate
+      }
+
+      if (!Object.is(newState, hook.memoizedState)) {
+        markWorkInProgressReceivedUpdate()
+      }
+
+      hook.memoizedState = newState
+      hook.baseUpdate = newBaseUpdate
+      hook.baseState = newBaseState
+
+      queue.eagerReducer = reducer
+      queue.eagerState = newState
+    }
+
+    const { dispatch } = queue
+    return [hook.memoizedState, dispatch]
   },
 }
 
